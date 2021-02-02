773cf5bc55de054248cb7c6710b69f18cb80515a
Andrey Sokolov <sokolov19830711@gmail.com>
2021-02-02 18:34:37 +0300

    INI file initialization was added. Text log Widget was added.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,19 +2,22 @@
 
 #include <QDebug>
 
+#include <QStandardPaths>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
+    _settings = QSharedPointer<QSettings>::create(QStandardPaths::writableLocation(QStandardPaths::ConfigLocation) + "/settings.ini", QSettings::IniFormat);
+
     QVBoxLayout* mainLayout = new QVBoxLayout;
     setLayout(mainLayout);
 
     QLabel* selectDeviceBoxLabel = new QLabel("Step 1. Select device model", this);
     _selectDeviceModelBox = new QComboBox(this);
     _selectDeviceModelBox->setFixedWidth(150);
-    _selectDeviceModelBox->addItems({"Olc Nema", "Olc Zhaga"});
+    _selectDeviceModelBox->addItems(_settings->value("Devices").toString().split("|"));
     connect(_selectDeviceModelBox, SIGNAL(currentTextChanged(const QString&)), this, SLOT(onSelectDeviceBoxCurrentTextChanged(const QString&)));
     QHBoxLayout* selectDeviceLayout = new QHBoxLayout;
     selectDeviceLayout->addWidget(selectDeviceBoxLabel);
@@ -23,6 +26,13 @@ MainWindow::MainWindow(QWidget *parent)
 
     mainLayout->addLayout(selectDeviceLayout);
     mainLayout->addStretch();
+
+    _logWidget = new QListWidget(this);
+    mainLayout->addWidget(_logWidget);
+
+    logInfo("Info string");
+    logError("error string");
+    logSuccess("Success string");
 }
 
 MainWindow::~MainWindow()
@@ -33,4 +43,33 @@ MainWindow::~MainWindow()
 void MainWindow::onSelectDeviceBoxCurrentTextChanged(const QString &text)
 {
     qDebug() << text << "device selected";
+}
+
+void MainWindow::logInfo(const QString &message)
+{
+    _logWidget->addItem(message);
+    _logWidget->scrollToBottom();
+    qInfo().noquote() << message;
+}
+
+void MainWindow::logError(const QString &message)
+{
+    QListWidgetItem *item = new QListWidgetItem(message);
+
+    item->setBackground(Qt::red);
+    item->setForeground(Qt::white);
+    _logWidget->addItem(item);
+    _logWidget->scrollToBottom();
+    qCritical().noquote() << message;
+}
+
+void MainWindow::logSuccess(const QString &message)
+{
+    QListWidgetItem *item = new QListWidgetItem(message);
+
+    item->setBackground(Qt::green);
+    item->setForeground(Qt::black);
+    _logWidget->addItem(item);
+    _logWidget->scrollToBottom();
+    qInfo().noquote() << message;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QSettings>
 #include <QComboBox>
+#include <QListWidget>
 #include <QLabel>
 
 class MainWindow : public QWidget
@@ -18,7 +20,15 @@ public slots:
 
 private:
 
+    void logInfo(const QString &message);
+    void logError(const QString &message);
+    void logSuccess(const QString &message);
+
+    QSharedPointer<QSettings> _settings;
+
+    //--- GUI Elements ------------------------------------------------
     QComboBox* _selectDeviceModelBox;
+    QListWidget *_logWidget;
 };
 
 #endif // MAINWINDOW_H
