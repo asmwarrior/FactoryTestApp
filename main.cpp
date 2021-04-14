8313460f89f2aa01ea68e6ab80329252ef6be1a9
Sergey Semernin <sergey.semernin@capelon.eu>
2021-04-14 11:20:24 +0300

    Version info now in version.h file.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "MainWindow.h"
+#include "version.h"
 
 #include <QApplication>
 
@@ -6,15 +7,14 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-//    a.setWindowIcon(QIcon(":/icons/application"));
     a.setOrganizationName("Capelon AB");
     a.setApplicationName("CapelonTestStation");
+    a.setApplicationVersion(CTS_VERSION);
 
     MainWindow w;
-    w.setWindowTitle("Capelon Test Station 0.6.45");
+    w.setWindowTitle("Capelon Test Station " + a.applicationVersion());
     w.setMinimumWidth(1200);
     w.setMinimumHeight(900);
-//    w.setFixedSize(1200, 900);
     w.show();
 
     return a.exec();
---
+++ version.h
@@ -0,0 +1,11 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define CTS_VER_MAJOR        0
+#define CTS_VER_MINOR        6
+#define CTS_VER_REVISION     46
+#define CTS_VER_BUILD        1
+
+#define CTS_VERSION          "0.6.46"
+
+#endif // __VERSION_H__
